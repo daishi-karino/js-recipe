85d2c83ef2d33fd39ecd4396bf31da95468f65c5
daishi-karino <dena-a4i7u23t32@ezweb.ne.jp>
2022-01-30 11:29:41 +0900

    ペア課題完成

--- 04_ゼミ/week0/session/week2/main.js
+++ 04_ゼミ/week0/session/week2/main.js
@@ -3,6 +3,30 @@ const memoInput = document.getElementById("memo-input")
 const memoContainer = document.getElementById("memo-container")
 
 addButton.onclick = function () {
+  const inputValue = createDiv(memoInput.value)
+  memoContainer.append(inputValue)
+
+  memoInput.value = ""
+}
+
+const createDiv = function (text) {
+  const div = document.createElement("div")
+  div.textContent = text
+  div.className = "div"
+
+  const deleteButton = document.createElement("button")
+  deleteButton.textContent = "削除"
+
+  deleteButton.onclick = function () {
+    div.remove()
+  }
+  div.append(deleteButton)
+
+  return div
+}
+
+/*
+addButton.onclick = function () {
   const inputValue = memoInput.value
   console.log(inputValue)
 
@@ -11,4 +35,15 @@ addButton.onclick = function () {
   memoContainer.append(div)
 
   memoInput.value = ""
+
+  const deleteButton = document.createElement("button")
+  deleteButton.textContent = "削除"
+
+  // 削除ボタンを押したときの処理を登録
+  deleteButton.onclick = function () {
+    // カードを削除する
+    div.remove()
+  }
+  div.append(deleteButton)
 }
+*/
