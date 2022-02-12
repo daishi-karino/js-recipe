35ea175986577fd784ef52e39d5d1c7260b69747
daishi-karino <dena-a4i7u23t32@ezweb.ne.jp>
2022-02-12 14:22:15 +0900

    VueATMをやりました

--- 02_レシピ/03_Vue ATM/main.js
+++ 02_レシピ/03_Vue ATM/main.js
@@ -1,7 +1,29 @@
 Vue.createApp({
   data() {
     return {
-      money: 0,
+      inputMoney: "",
+      zandaka: 0,
+      records: [{}],
     }
+  },
+  methods: {
+    nyukin: function () {
+      this.zandaka += Number(this.inputMoney)
+      const now = new Date()
+      this.records.push({ date: now, type: "入金", money: this.inputMoney })
+    },
+    syukkin: function () {
+      this.zandaka -= Number(this.inputMoney)
+      const now = new Date()
+      console.log(now)
+      this.records.push({ date: now, type: "出金", money: this.inputMoney })
+    },
+  },
+  computed: {
+    disabled: function () {
+      if (this.zandaka < this.inputMoney) {
+        return true
+      }
+    },
   },
 }).mount("#app")
